93cad1a131c8d638f547d17525f0051234b22d62
123 <123>
2026-02-26 09:34:40 +0200

    asdasdas

--- api/index.php
+++ api/index.php
@@ -1,1 +1,17 @@
-require __DIR__ . "/../public/index.php";
+<?php
+
+// Головний вхід для Vercel
+require __DIR__ . '/../vendor/autoload.php';
+
+// Запуск Laravel
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
